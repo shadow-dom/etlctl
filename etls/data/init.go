e818c13ef203803c13fca94662d33852b17a1c44
shadow-dom <jacob.r.holcomb+1@gmail.com>
2025-02-15 09:05:06 -0500

    Added example DBs and init script.

---
+++ etls/data/init.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const SCHEMA = `
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	toppings TEXT NOT NULL
+`
+
+func InitDB(name string, insertDummyData bool) {
+	fileName := name + ".db"
+
+	if _, err := os.Stat(fileName); errors.Is(err, os.ErrNotExist) {
+		db, err := sql.Open("sqlite3", fileName)
+		if err != nil {
+			log.Fatalf("Failed to open %s: %v", fileName, err)
+		}
+		defer db.Close()
+
+		sql := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (%s);", name, SCHEMA)
+
+		_, err = db.Exec(sql)
+		if err != nil {
+			log.Fatalf("Failed to create %s table: %v", name, err)
+		}
+
+		if insertDummyData {
+			sql = fmt.Sprintf(`
+			INSERT INTO %s (name, toppings) VALUES
+				('Margherita', 'Tomato, Mozzarella, Basil'),
+				('Pepperoni', 'Tomato, Mozzarella, Pepperoni'),
+				('BBQ Chicken', 'BBQ Sauce, Chicken, Onion, Cheese')
+			ON CONFLICT DO NOTHING;
+			`, name)
+
+			_, err = db.Exec(sql)
+			if err != nil {
+				log.Fatalf("Failed to insert dummy data into %s table: %v", name, err)
+			}
+		}
+
+		fmt.Printf("%s database set up successfully.\n", name)
+	} else {
+		fmt.Printf("Skipping... %s database already exists.\n", name)
+	}
+}
+
+func main() {
+	InitDB("pizza", true)
+	InitDB("delivery", false)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.4
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
