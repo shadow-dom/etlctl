d4956babaa5ad755f1e7d694b1c193619d53c91b
shadow-dom <jacob.r.holcomb+1@gmail.com>
2025-03-02 02:01:41 -0500

    Full generation of first ETL.

--- pkg/util/etl/gen/multiple-pizza-shops.go
+++ pkg/util/etl/gen/multiple-pizza-shops.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-    "fmt"
 	"shadow-dom/etlctl/pkg/util/etl"
 )
 
 func main() {
-	var etl etl.ETL = etl.ETL {
+	var etl etl.ETL = etl.ETL{
         Sources: []etl.DBStorage{
 			
 			{
-				Name: "pizza_db",
-				Type: "sqlite3",
-				Connection: map[string]string{"filepath": `../etls/data/pizza.db`},
+				Name:       "pizza_db",
+				Type:       "sqlite3",
+				Connection: map[string]string{"filepath": `~/apps/projects/etlctl/etls/data/pizza.db`},
 			},
-			
 			{
-				Name: "pizza_2_db",
-				Type: "sqlite3",
-				Connection: map[string]string{"filepath": `../etls/data/pizza_2.db`},
+				Name:       "pizza_2_db",
+				Type:       "sqlite3",
+				Connection: map[string]string{"filepath": `~/apps/projects/etlctl/etls/data/pizza_2.db`},
 			},
-			
 		},
 		Queries: []etl.Query{
 			
@@ -29,16 +26,14 @@ func main() {
 				SQL: `SELECT name, toppings FROM pizza;
 `,
 			},
-			
 		},
 		Targets: []etl.DBStorage{
 			
 			{
-				Name: "delivery_db",
-				Type: "sqlite3",
-				Connection: map[string]string{"filepath": `../etls/data/delivery.db`},
+				Name:       "delivery_db",
+				Type:       "sqlite3",
+				Connection: map[string]string{"filepath": `~/apps/projects/etlctl/etls/data/delivery.db`},
 			},
-			
 		},
 		Pipelines: []etl.Pipeline{
 			
@@ -47,12 +42,11 @@ func main() {
 				Query:   "get_pizzas",
 				Target:  "delivery_db.delivery",
 				Fields: []etl.FieldMapping{
-					{Source: "name", Target: "name"},{Source: "toppings", Target: "toppings"},
+					{Source: "name", Target: "name"}, {Source: "toppings", Target: "toppings"}, 
 				},
 			},
-			
 		},
 	}
 
-    fmt.Println(etl)
+    etl.Run()
 }
